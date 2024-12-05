bf4ac3bc5cc81d91c6e900facd0449b2da83d889
andres1996u <trejos_1996@hotmail.com>
2024-12-05 10:05:47 -0500

    05/12/2024
    Commit detalles de la orden.

---
+++ e-commerce-project/frontend/src/components/Order/OrderDetails.jsx
@@ -0,0 +1,123 @@
+import { useSnackbar } from 'notistack';
+import { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { useParams } from 'react-router-dom';
+import { clearErrors, getOrderDetails } from '../../actions/orderActions'; // Actualizar la ruta de la acción
+import Loader from '../Layouts/Loader';
+import TrackStepper from './TrackStepper';
+import MinCategory from '../Layouts/MinCategory';
+import MetaData from '../Layouts/MetaData';
+
+const OrderDetails = () => {
+    const dispatch = useDispatch();
+    const { enqueueSnackbar } = useSnackbar();
+    const params = useParams();
+
+    const { order, error, loading } = useSelector((state) => state.orderDetails); // Asegúrate de que el reducer está configurado correctamente.
+
+    useEffect(() => {
+        if (error) {
+            enqueueSnackbar(error, { variant: 'error' });
+            dispatch(clearErrors());
+        }
+        dispatch(getOrderDetails(params.id));
+    }, [dispatch, error, params.id, enqueueSnackbar]);
+
+    return (
+        <>
+            <MetaData title="Order Details | E-commerce Final" />
+
+            <MinCategory />
+            <main className="w-full mt-14 sm:mt-4">
+                {loading ? (
+                    <Loader />
+                ) : (
+                    <>
+                        {order && order.client && order.shippingInfo && (
+                            <div className="flex flex-col gap-4 max-w-6xl mx-auto">
+                                {/* Delivery Address */}
+                                <div className="flex bg-white shadow rounded-sm min-w-full">
+                                    <div className="sm:w-1/2 border-r">
+                                        <div className="flex flex-col gap-3 my-8 mx-10">
+                                            <h3 className="font-medium text-lg">Dirección de entrega</h3>
+                                            <h4 className="font-medium">{order.client.firstName} {order.client.lastName}</h4>
+                                            <p className="text-sm">{`${order.shippingInfo.address}, ${order.shippingInfo.city}, ${order.shippingInfo.state} - ${order.shippingInfo.postalCode}`}</p>
+                                            <div className="flex gap-2 text-sm">
+                                                <p className="font-medium">Email:</p>
+                                                <p>{order.client.email}</p>
+                                            </div>
+                                            <div className="flex gap-2 text-sm">
+                                                <p className="font-medium">Número teléfonico:</p>
+                                                <p>{order.shippingInfo.phone}</p>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+
+                                {/* Pedido de articulos */}
+                                {order.orderItems &&
+                                    order.orderItems.map((item) => {
+                                        const { _id, productImage, productName, unitPrice, quantity } = item;
+
+                                        return (
+                                            <div
+                                                className="flex flex-col sm:flex-row min-w-full shadow rounded-sm bg-white px-2 py-5"
+                                                key={_id}
+                                            >
+                                                {/* Product Details */}
+                                                <div className="flex flex-col sm:flex-row sm:w-1/2 gap-2">
+                                                    <div className="w-full sm:w-32 h-20">
+                                                        <img
+                                                            draggable="false"
+                                                            className="h-full w-full object-contain"
+                                                            src={productImage}
+                                                            alt={productName}
+                                                        />
+                                                    </div>
+                                                    <div className="flex flex-col gap-1 overflow-hidden">
+                                                        <p className="text-sm">
+                                                            {productName.length > 60
+                                                                ? `${productName.substring(0, 60)}...`
+                                                                : productName}
+                                                        </p>
+                                                        <p className="text-xs text-gray-600 mt-2">
+                                                            Cantidad: {quantity}
+                                                        </p>
+                                                        <p className="text-xs text-gray-600">
+                                                            Precio: ₹{unitPrice.toLocaleString()}
+                                                        </p>
+                                                        <span className="font-medium">
+                                                            Total: ₹{(quantity * unitPrice).toLocaleString()}
+                                                        </span>
+                                                    </div>
+                                                </div>
+
+                                                {/* Estado del pedido */}
+                                                <div className="flex flex-col w-full sm:w-1/2">
+                                                    <h3 className="font-medium sm:text-center">Estado pedido</h3>
+                                                    <TrackStepper
+                                                        orderOn={order.createdAt}
+                                                        shippedAt={order.shippedAt}
+                                                        deliveredAt={order.deliveredAt}
+                                                        activeStep={
+                                                            order.orderStatus === 'Delivered'
+                                                                ? 2
+                                                                : order.orderStatus === 'Shipped'
+                                                                ? 1
+                                                                : 0
+                                                        }
+                                                    />
+                                                </div>
+                                            </div>
+                                        );
+                                    })}
+                            </div>
+                        )}
+                    </>
+                )}
+            </main>
+        </>
+    );
+};
+
+export default OrderDetails;
