b5a695a858a786f87ced4eef20d8a8f629f712d4
Anderson_RS <ander.irs@gmail.com>
2024-12-06 11:08:19 -0500

    feat: Modifying React components for Sign-up and login

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,27 +1,3 @@
-// App.js
-import React, { useState, useEffect } from 'react';
-import Header from './components/Header';
-import ProductList from './components/ProductList';
-import AdminPanel from './components/AdminPanel';
-import Footer from './components/Footer';
-import './App.css';
 
-const App = () => {
-  const [cart, setCart] = useState([]);
-  const [currentPage, setCurrentPage] = useState('products');
-
-  return (
-    <div className="app">
-      <Header cart={cart} setCurrentPage={setCurrentPage} />
-      {currentPage === 'products' && (
-        <ProductList cart={cart} setCart={setCart} />
-      )}
-      {currentPage === 'admin' && (
-        <AdminPanel />
-      )}
-      <Footer />
-    </div>
-  );
-};
 
 export default App;
