a3c044c917ed61892f2ad7a93c74bcbf828e725f
andres1996u <trejos_1996@hotmail.com>
2024-12-06 03:38:34 -0500

    Actualización
    Modificaciones de usuario

--- e-commerce-project/frontend/src/components/User/UpdatePassword.jsx
+++ e-commerce-project/frontend/src/components/User/UpdatePassword.jsx
@@ -1,75 +1,64 @@
 import { useEffect, useState } from 'react';
 import TextField from '@mui/material/TextField';
-import { useSnackbar } from 'notistack';
 import { Link, useNavigate } from 'react-router-dom';
 import { useDispatch, useSelector } from 'react-redux';
-import { clearErrors, loadUser, updatePassword } from '../../actions/userActions'; // Ajuste de ruta
+import { updatePassword, clearErrors, loadUser } from '../../actions/userAction';
 import { UPDATE_PASSWORD_RESET } from '../../constants/userConstants';
-import BackdropLoader from '../Layouts/BackdropLoader';
 import MetaData from '../Layouts/MetaData';
-import FormSidebar from './FormSidebar';
 
 const UpdatePassword = () => {
     const dispatch = useDispatch();
     const navigate = useNavigate();
-    const { enqueueSnackbar } = useSnackbar();
+    // const { enqueueSnackbar } = useSnackbar();
 
     const { error, isUpdated, loading } = useSelector((state) => state.profile);
 
-    const [oldPassword, setOldPassword] = useState('');
-    const [newPassword, setNewPassword] = useState('');
-    const [confirmPassword, setConfirmPassword] = useState('');
+    const [oldPassword, setOldPassword] = useState("");
+    const [newPassword, setNewPassword] = useState("");
+    const [confirmPassword, setConfirmPassword] = useState("");
 
     const updatePasswordSubmitHandler = (e) => {
         e.preventDefault();
 
-        if (newPassword.length < 8) {
-            enqueueSnackbar('Password length must be at least 8 characters', { variant: 'warning' });
-            return;
-        }
-        if (newPassword !== confirmPassword) {
-            enqueueSnackbar("Passwords don't match", { variant: 'error' });
-            return;
-        }
+        // if (newPassword.length < 8) {
+        //     enqueueSnackbar("Password length must be at least 8 characters", { variant: "warning" });
+        //     return;
+        // }
+        // if (newPassword !== confirmPassword) {
+        //     enqueueSnackbar("Passwords do not match", { variant: "error" });
+        //     return;
+        // }
 
         const formData = new FormData();
-        formData.set('oldPassword', oldPassword);
-        formData.set('newPassword', newPassword);
-        formData.set('confirmPassword', confirmPassword);
+        formData.set("oldPassword", oldPassword);
+        formData.set("newPassword", newPassword);
+        formData.set("confirmPassword", confirmPassword);
 
         dispatch(updatePassword(formData));
     };
 
     useEffect(() => {
-        if (error) {
-            enqueueSnackbar(error, { variant: 'error' });
-            dispatch(clearErrors());
-        }
-        if (isUpdated) {
-            enqueueSnackbar('Password updated successfully', { variant: 'success' });
-            dispatch(loadUser());
-            navigate('/account');
-
-            dispatch({ type: UPDATE_PASSWORD_RESET });
-        }
-    }, [dispatch, error, isUpdated, navigate, enqueueSnackbar]);
+        // if (error) {
+        //     enqueueSnackbar(error, { variant: "error" });
+        //     dispatch(clearErrors());
+        // }
+        // if (isUpdated) {
+        //     enqueueSnackbar("Password Updated Successfully", { variant: "success" });
+        //     dispatch(loadUser());
+        //     navigate('/account');
+        //     dispatch({ type: UPDATE_PASSWORD_RESET });
+        // }
+    }, [dispatch, error, isUpdated, navigate]);
 
     return (
         <>
-            <MetaData title="Update Password | E-commerce Final" />
+            <MetaData title="Password Update | Simplified" />
 
-            {loading && <BackdropLoader />}
+            {/* {loading && <BackdropLoader />} */}
             <main className="w-full mt-12 sm:pt-20 sm:mt-0">
                 <div className="flex sm:w-4/6 sm:mt-4 m-auto mb-7 bg-white shadow-lg">
-                    <FormSidebar
-                        title="Update Password"
-                        tag="Enter your current and new password to update"
-                    />
-
                     <div className="flex-1 overflow-hidden">
-                        <h2 className="text-center text-2xl font-medium mt-6 text-gray-800">
-                            Update Password
-                        </h2>
+                        <h2 className="text-center text-2xl font-medium mt-6 text-gray-800">Update Password</h2>
                         <form
                             onSubmit={updatePasswordSubmitHandler}
                             className="p-5 sm:p-14"
@@ -104,18 +93,8 @@ const UpdatePassword = () => {
                                         required
                                     />
                                 </div>
-                                <button
-                                    type="submit"
-                                    className="text-white py-3 w-full bg-primary-orange shadow hover:shadow-lg rounded-sm font-medium"
-                                >
-                                    Update
-                                </button>
-                                <Link
-                                    className="hover:bg-gray-50 text-primary-blue text-center py-3 w-full shadow border rounded-sm font-medium mb-8"
-                                    to="/account"
-                                >
-                                    Cancel
-                                </Link>
+                                <button type="submit" className="text-white py-3 w-full bg-primary-orange shadow hover:shadow-lg rounded-sm font-medium">Update</button>
+                                <Link className="hover:bg-gray-50 text-primary-blue text-center py-3 w-full shadow border rounded-sm font-medium mb-8" to="/account">Cancel</Link>
                             </div>
                         </form>
                     </div>
--- e-commerce-project/frontend/src/components/User/UpdateProfile.jsx
+++ e-commerce-project/frontend/src/components/User/UpdateProfile.jsx
@@ -1,90 +1,59 @@
 import { useEffect, useState } from 'react';
 import TextField from '@mui/material/TextField';
-import { Avatar, FormControlLabel, Radio, RadioGroup } from '@mui/material';
 import { useSnackbar } from 'notistack';
 import { Link, useNavigate } from 'react-router-dom';
 import { useDispatch, useSelector } from 'react-redux';
-import { clearErrors, loadUser, updateProfile } from '../../actions/userActions'; // Ajuste de ruta
+import { clearErrors, loadUser, updateProfile } from '../../actions/userAction';
 import { UPDATE_PROFILE_RESET } from '../../constants/userConstants';
-import BackdropLoader from '../Layouts/BackdropLoader';
 import MetaData from '../Layouts/MetaData';
-import FormSidebar from './FormSidebar';
 
 const UpdateProfile = () => {
     const dispatch = useDispatch();
     const navigate = useNavigate();
-    const { enqueueSnackbar } = useSnackbar();
+    // const { enqueueSnackbar } = useSnackbar();
 
     const { user } = useSelector((state) => state.user);
-    const { error, isUpdated, loading } = useSelector((state) => state.profile);
+    const { error, isUpdated } = useSelector((state) => state.profile);
 
-    const [name, setName] = useState('');
-    const [email, setEmail] = useState('');
-    const [gender, setGender] = useState('');
-    const [avatar, setAvatar] = useState('');
-    const [avatarPreview, setAvatarPreview] = useState('');
+    const [name, setName] = useState("");
+    const [email, setEmail] = useState("");
 
     const updateProfileHandler = (e) => {
         e.preventDefault();
 
         const formData = new FormData();
-        formData.set('name', name);
-        formData.set('email', email);
-        formData.set('gender', gender);
-        formData.set('avatar', avatar);
+        formData.set("name", name);
+        formData.set("email", email);
 
         dispatch(updateProfile(formData));
-    };
-
-    const handleUpdateDataChange = (e) => {
-        const reader = new FileReader();
-        setAvatar('');
-        reader.onload = () => {
-            if (reader.readyState === 2) {
-                setAvatarPreview(reader.result);
-                setAvatar(reader.result);
-            }
-        };
-
-        reader.readAsDataURL(e.target.files[0]);
     };
 
     useEffect(() => {
         if (user) {
             setName(user.name);
             setEmail(user.email);
-            setGender(user.gender);
-            setAvatarPreview(user.avatar.url);
         }
-        if (error) {
-            enqueueSnackbar(error, { variant: 'error' });
-            dispatch(clearErrors());
-        }
-        if (isUpdated) {
-            enqueueSnackbar('Profile Updated Successfully', { variant: 'success' });
-            dispatch(loadUser());
-            navigate('/account');
-
-            dispatch({ type: UPDATE_PROFILE_RESET });
-        }
-    }, [dispatch, error, user, isUpdated, navigate, enqueueSnackbar]);
+        // if (error) {
+        //     enqueueSnackbar(error, { variant: "error" });
+        //     dispatch(clearErrors());
+        // }
+        // if (isUpdated) {
+        //     enqueueSnackbar("Profile Updated Successfully", { variant: "success" });
+        //     dispatch(loadUser());
+        //     navigate('/account');
+        //     dispatch({ type: UPDATE_PROFILE_RESET });
+        // }
+    }, [dispatch, error, user, isUpdated, navigate]);
 
     return (
         <>
-            <MetaData title="Update Profile | E-commerce Final" />
-
-            {loading && <BackdropLoader />}
+            <MetaData title="Update Profile | Simplified" />
             <main className="w-full mt-12 sm:pt-20 sm:mt-0">
                 <div className="flex sm:w-4/6 sm:mt-4 m-auto mb-7 bg-white shadow-lg">
-                    <FormSidebar
-                        title="Update Profile"
-                        tag="Update your personal information and avatar"
-                    />
                     <div className="flex-1 overflow-hidden">
                         <h2 className="text-center text-2xl font-medium mt-6 text-gray-800">Update Profile</h2>
                         <form
                             onSubmit={updateProfileHandler}
-                            encType="multipart/form-data"
                             className="p-5 sm:p-10"
                         >
                             <div className="flex flex-col gap-4 items-start">
@@ -107,62 +76,8 @@ const UpdateProfile = () => {
                                         required
                                     />
                                 </div>
-                                <div className="flex gap-4 items-center">
-                                    <h2 className="text-md">Your Gender:</h2>
-                                    <div className="flex items-center gap-6">
-                                        <RadioGroup
-                                            row
-                                            aria-labelledby="radio-buttons-group-label"
-                                            name="radio-buttons-group"
-                                        >
-                                            <FormControlLabel
-                                                name="gender"
-                                                value="male"
-                                                checked={gender === 'male'}
-                                                onChange={(e) => setGender(e.target.value)}
-                                                control={<Radio required />}
-                                                label="Male"
-                                            />
-                                            <FormControlLabel
-                                                name="gender"
-                                                value="female"
-                                                checked={gender === 'female'}
-                                                onChange={(e) => setGender(e.target.value)}
-                                                control={<Radio required />}
-                                                label="Female"
-                                            />
-                                        </RadioGroup>
-                                    </div>
-                                </div>
-                                <div className="flex flex-col w-full gap-3 items-center">
-                                    <Avatar
-                                        alt="Avatar Preview"
-                                        src={avatarPreview}
-                                        sx={{ width: 56, height: 56 }}
-                                    />
-                                    <label className="rounded font-medium bg-gray-400 text-center cursor-pointer text-white w-full py-2 px-2.5 shadow hover:shadow-lg">
-                                        <input
-                                            type="file"
-                                            name="avatar"
-                                            accept="image/*"
-                                            onChange={handleUpdateDataChange}
-                                            className="hidden"
-                                        />
-                                        Choose File
-                                    </label>
-                                </div>
-                                <button
-                                    type="submit"
-                                    className="text-white py-3 w-full bg-primary-orange shadow rounded-sm font-medium hover:shadow-lg"
-                                >
-                                    Update
-                                </button>
-                                <Link
-                                    className="hover:bg-gray-100 text-primary-blue text-center py-3 w-full shadow border rounded-sm font-medium"
-                                    to="/account"
-                                >
-                                    Cancel
-                                </Link>
+                                <button type="submit" className="text-white py-3 w-full bg-primary-orange shadow hover:shadow-lg rounded-sm font-medium">Update</button>
+                                <Link className="hover:bg-gray-100 text-primary-blue text-center py-3 w-full shadow border rounded-sm font-medium" to="/account">Cancel</Link>
                             </div>
                         </form>
                     </div>
